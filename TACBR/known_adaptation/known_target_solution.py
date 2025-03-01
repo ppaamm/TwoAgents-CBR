4a74a8ac15a022fd418dd9896044a11c334da5f5
Pierre-Alexandre Murena <61550406+ppaamm@users.noreply.github.com>
2025-03-01 17:34:06 +0100

    Implementation of a naive solution-oriented retrieval

---
+++ TACBR/known_adaptation/known_target_solution.py
@@ -0,0 +1,27 @@
+from CBR.CaseBase import Case, CaseBase
+from CBR.containers import Retrieval, Adaptation
+from typing import List, Any, Dict, Optional
+import itertools
+
+
+class DirectingRetrieval(Retrieval):
+    def __init__(self, parameters: Dict[str, Any]):
+        assert "adaptation" in parameters, "'adaptation' key must be present in parameters"
+        assert isinstance(parameters["adaptation"], Adaptation), "'adaptation' must be of type Adaptation"
+        super().__init__(parameters)
+        
+        assert "distance" in parameters, "'distance' key must be present in parameters"
+        distance_function = parameters["distance"]
+        assert callable(parameters["distance"]), "'distance' must be a callable function"
+        assert len(parameters["distance"].__code__.co_varnames) == 2, "'distance' function must accept exactly two arguments"
+
+
+
+    def retrieve(self, target_problem: Any, CB: CaseBase, K) -> List[Case]:
+        distance = self.parameters["distance"]
+        adaptation = self.parameters["adaptation"]
+        dist_ad = lambda C: distance(target_problem.solution, 
+                                     adaptation.adapt(C, target_problem.problem))
+        cases = min(itertools.combinations(CB, K), key=dist_ad)
+        return cases
+            
--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-from CBR.CaseBase import Case, CaseBase
-from CBR import containers
-from morphologicalCBR.TextCase import TextCase, TextCaseBase
-
+from morphologicalCBR.TextCase import TextCase
 from morphologicalCBR.adaptation import VowelHarmonyAdaptation
 from morphologicalCBR.retrieval import MorphologicalRetrieval, problem_distance
 
+from TACBR.known_adaptation.known_target_solution import DirectingRetrieval
+
+
+
+# Testing CBR for morphology
 
 CB = [TextCase("kyy", "kyyllä"), 
       TextCase("sota", "sodalla"), 
       TextCase("koira", "koiralla")]
 
-
 ret = MorphologicalRetrieval({"distance": problem_distance})
 cases = ret.retrieve("hyvä", CB, K=2)
-
 ad = VowelHarmonyAdaptation({"knows_harmony": True})
-
 print(ad.adapt(cases, "hyvä"))
+
+
+
+# Testing retrieval with known target solution
+
+def naive_text_distance(t1, t2):
+    if t1 == t2: return 0
+    return 1 + abs(len(t1) - len(t2))
+
+VH_adaptation = VowelHarmonyAdaptation({"knows_harmony": True})
+
+
+dir_ret_parameters = { "distance": naive_text_distance, 
+                       "adaptation": VH_adaptation}
+
+dir_ret = DirectingRetrieval(dir_ret_parameters)
+print(dir_ret.retrieve(TextCase("hyvä", "hyvällä"), CB, 1))
+
