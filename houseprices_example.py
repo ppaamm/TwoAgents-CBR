afb75d600d287c94a29ab024cbaaea455f7637f5
Pierre-Alexandre Murena <61550406+ppaamm@users.noreply.github.com>
2025-03-10 10:34:25 +0100

    Implementation of adaptation inference with normal noise

--- houseprices_example.py
+++ houseprices_example.py
@@ -10,6 +10,7 @@ from numericalCBR.numerical_case import NumericalCase, NumericalCaseBase
 from numericalCBR.retrieval import WeightedDistanceRetrieval, LearnableWeightedDistanceRetrieval
 
 from TACBR.known_adaptation.known_target_solution import DirectingRetrieval
+from TACBR.unknown_adaptation.retrieval import FiniteAdaptationProbability, UnknownFiniteAdaptationRetrieval
 
 
 # Load California housing dataset
@@ -82,16 +83,42 @@ case_ret = dir_ret.retrieve(NumericalCase(x_tgt, y_tgt), CB_list, 2)
 parameters = {'retrieval': WeightedDistanceRetrieval({}), 'adaptation': w_adaptation, 
               #'optimization_method': 'grid',}
               'optimization_method': 'pso',}
-retrieval = LearnableWeightedDistanceRetrieval(parameters)
+learnable_weight_retrieval = LearnableWeightedDistanceRetrieval(parameters)
 
 CB_test = NumericalCaseBase.from_numpy(X_test, y_test)
 fit_params = {'bounds': (0, 1), 'K': 2, 
-              'optimization_params': { 'max_iter': 50, 'num_particles': 30, 
+              'optimization_params': { 'max_iter': 5, 'num_particles': 5, 
                                       'w': 0.8, 'c1': 5, 'c2': 1 } }
               #'optimization_params': { 'num_samples': 2, 'n_verbose': 100 } }
 
-retrieval.fit(CB, CB_test, quadratic_distance, fit_params)
+learnable_weight_retrieval.fit(CB, CB_test, quadratic_distance, fit_params)
 
 
 
 
+# UnknownAdaptationRetrieval
+
+probabilities = [(WeightedAdaptation({"weight": np.array([1., 0.])}), 0.4), 
+                 (WeightedAdaptation({"weight": np.array([0., 0.])}), 0.4), 
+                 (WeightedAdaptation({"weight": np.array([.5, .5])}), 0.2)]
+prior = FiniteAdaptationProbability(probabilities)
+
+retrieval = UnknownFiniteAdaptationRetrieval({"adaptation_probability": prior})
+
+def normal_likelihood(observations, adaptation, sigma2=1e-5):
+    N = len(observations)
+    residuals = np.array([observations[n][1] - adaptation.adapt(observations[n][2], observations[n][0]) for n in range(N)])
+    log_likelihood_value = - (N / 2) * np.log(2 * np.pi * sigma2) - np.sum(residuals**2) / (2 * sigma2)
+    return np.exp(log_likelihood_value)
+
+n_obs = 10
+observations = []
+for n in range(n_obs):
+    x = X_train[n,:]
+    cases = learnable_weight_retrieval.retrieve(x, CB_test, 2)
+    y = w_adaptation.adapt(cases, x)
+    observations.append((x, y, cases))
+
+
+retrieval.update_adaptation_probability(observations, normal_likelihood)
+print(retrieval.adaptation_probability.adaptation_probabilities)
